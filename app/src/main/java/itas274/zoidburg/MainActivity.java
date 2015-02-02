c877ad9f5f10525fce2525ce0e92ad6ac57b0475
Johnathan <Johnathan.hall@itas.ca>
2015-02-02 11:36:35 -0800

    Commit Test

---
+++ app/src/main/java/itas274/zoidburg/MainActivity.java
@@ -0,0 +1,35 @@
+package itas274.zoidburg;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+    public void testPlay(View view) {
+        Context context = getApplicationContext();
+        MediaPlayer mediaPlayer = MediaPlayer.create(context, R.raw.whoop_whoop);
+        mediaPlayer.start();
+    }
+
+}
